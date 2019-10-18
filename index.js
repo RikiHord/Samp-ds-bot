ea8810cd8d9bd2e42457c1cef73f4b2163764bf7
RikiHord <47187306+RikiHord@users.noreply.github.com>
2019-10-18 20:38:27 +0300

    Update index.js

--- index.js
+++ index.js
@@ -91,7 +91,8 @@ bot.on("message", async message => {
   }
 
   let prefix = option.prefix;
-  let args = message.content.slice(prefix.length).trim().split(' ');
+  let args = message.content.substr(prefix.length).split(" ");
+  //let args = message.content.slice(prefix.length).trim().split(' ');
   let cmd = args.shift().toLowerCase();
 
   if(message.author.bot) return;
