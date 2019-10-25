0e308b6e129fef0d6c41f63ee2397ec66c161a9f
RikiHord <47187306+RikiHord@users.noreply.github.com>
2019-10-25 14:39:27 +0300

    Update info.js

--- commands/info.js
+++ commands/info.js
@@ -7,7 +7,7 @@ module.exports.run = async (bot, message, args, db) => {
     let id = message.author.id;
 
     //Поиск игрока по id что бы получить всю информацию из бд
-    let sql = `SELECT * FROM users WHERE id_user = ${id}`;
+    let sql = `SELECT * FROM users WHERE id_user = ${`'`+id+`'`}`;
     db.query(sql, (err, result) => {
         if (err) {
             console.error(err.message);
@@ -16,11 +16,11 @@ module.exports.run = async (bot, message, args, db) => {
         if(result == undefined) notreg(message); //Игрок не зарегестрирован
         else{
         //Поиск названия фракции по fraction_id игрока
-        let sql = `SELECT * FROM fractions WHERE fraction_id = ${result.fraction_id}`;
-        db.query(sql, (err, result2) => {
-            if (err) {
-                console.error(err.message);
-            }
+        //let sql = `SELECT * FROM fractions WHERE fraction_id = ${result.fraction_id}`;
+        //db.query(sql, (err, result2) => {
+            //if (err) {
+                //console.error(err.message);
+            //}
 
                 //Список информации о игроке
                 let taggedUser = message.mentions.users.first() || message.author;
@@ -31,10 +31,10 @@ module.exports.run = async (bot, message, args, db) => {
                 .addField(`Уровень`, `${result.lvl}`)
                 .addField(`Баланс`, `${result.money}$`)
                 .addField(`Локация`, `${result.location}`)
-                .addField(`Фракция`, `${result2.fraction_name}`)
+                //.addField(`Фракция`, `${result2.fraction_name}`)
 
                 message.author.send(embed);
-        });
+        //});
         }
     });
 }
